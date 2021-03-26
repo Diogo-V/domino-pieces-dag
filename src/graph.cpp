4aec4373dcf1c83b61cf4ca9d36edcb93bac7fbb
Diogo-V <diogo.venancio00@gmail.com>
2021-03-26 14:58:39 +0000

    implemented some doc + functions

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,24 +1,92 @@
 #include <iostream>
 #include "graph.h"
 
+
 using namespace std;
 
+
+/**
+ * @brief Inserts a new edge from parent to child node.
+ * 
+ * @param parent parent's node
+ * @param child child's node
+ */
 void Graph::addEdge(int parent, int child) {
-    adjacent[parent].push_back(child); // Add w to node’s list.
+    _adjacent[parent].push_back(child); // Add w to node’s list.
 }
 
 
+/**
+ * @brief Performs a DFS traversal of this graph starting from root node.
+ * 
+ * @param root starting point for DFS traversal
+ */
 void Graph::DFS(int node) {
 
     // Mark the current node as visited and print it
-    node_info[node].color = Color::white;
+    this->setNodeColor(node, Color::white);
     cout << node << " ";
  
     // Recur for all the vertices adjacent to this vertex
-    for (list<int>::iterator i = adjacent[node].begin(); i != adjacent[node].end(); ++i) {
+    for (list<int>::iterator i = this->getAdjacentNodes(node).begin(); i != this->getAdjacentNodes(node).end(); ++i) {
         cout << *i << endl;
-        if (!(node_info[*i].color == Color::white))
+        if (!(this->getNodeInfo(*i).color == Color::white))
             DFS(*i);
     }
 
+}
+
+
+/**
+ * @brief Get the Node Info object.
+ * 
+ * @param node node value
+ * @return node_info_struct related to this node
+ */
+node_info_struct Graph::getNodeInfo(int node) { 
+    return this->_nodeInfo[node]; 
+}
+
+
+/**
+ * @brief Get the Adjacent Nodes object.
+ * 
+ * @param node node value
+ * @return list<int> representing a list of connected nodes
+ */
+list<int> Graph::getAdjacentNodes(int node) { 
+    return this->_adjacent[node];
+}
+
+
+/**
+ * @brief Changes node's starting time.
+ * 
+ * @param node node to be changed
+ * @param start_time new value
+ */
+void Graph::setNodeStartTime(int node, int start_time) {
+    this->_nodeInfo[node].start_time = start_time;
+}
+
+
+/**
+ * @brief Changes node's closing time.
+ * 
+ * @param node node to be changed
+ * @param finish_time new value
+ */
+void Graph::setNodeFinishTime(int node, int finish_time) {
+    this->_nodeInfo[node].finish_time = finish_time;
+}
+
+
+/**
+ * @brief Changes node's current color.
+ * 
+ * @param node node to be changed
+ * @param color new color
+ */
+void Graph::setNodeColor(int node, Color color) {
+    this->_nodeInfo[node].color = color;
 }
--- src/graph.h
+++ src/graph.h
@@ -1,5 +1,6 @@
 #ifndef GRAPH_H
 #define GRAPH_H
+
 #include <bits/stdc++.h>
 #include <string>
 
@@ -7,9 +8,23 @@
 using namespace std;
 
 
+/**
+ * @brief Represents a node's color. Used mainly during DFS.
+ * 
+ * @param white node has not been reached 
+ * @param grey node has been reached but not closed 
+ * @param black node has been closed
+ */
 enum class Color { white, black, grey };
 
 
+/**
+ * @brief Holds DFS information about the related node.
+ * 
+ * @param start_time int value representing the time of discovery of this node
+ * @param finish_time int value representing the time of closing of this node
+ * @param color represents the current state of the node
+ */
 typedef struct {
     int start_time;
     int finish_time;
@@ -17,22 +32,81 @@ typedef struct {
 } node_info_struct;
 
 
-// Graph class represents a directed graph
-// using adjacency list representation
+/**
+ * @brief Represents a Directed Acyclic Graph. Uses adjacency list.
+ */
 class Graph {
+    
+    private:
+
+        /**
+         * @brief Holds all the info related to the key node after DFS traversal.
+         */
+        map<int, node_info_struct> _nodeInfo;
+
+        /**
+         * @brief Holds all the nodes which this node leads to.
+         */
+        map<int, list<int>> _adjacent;
     
     public:
 
-        map<int, node_info_struct> node_info;
-        map<int, list<int>> adjacent;
+        /**
+         * @brief Get the Node Info object.
+         * 
+         * @param node node value
+         * @return node_info_struct related to this node
+         */
+        node_info_struct getNodeInfo(int node);
+
+        /**
+         * @brief Get the Adjacent Nodes object.
+         * 
+         * @param node node value
+         * @return list<int> representing a list of connected nodes
+         */
+        list<int> getAdjacentNodes(int node);
+        
+        /**
+         * @brief Changes node's starting time.
+         * 
+         * @param node node to be changed
+         * @param start_time new value
+         */
+        void setNodeStartTime(int node, int start_time);
+
+        /**
+         * @brief Changes node's closing time.
+         * 
+         * @param node node to be changed
+         * @param finish_time new value
+         */
+        void setNodeFinishTime(int node, int finish_time);
+
+        /**
+         * @brief Changes node's current color.
+         * 
+         * @param node node to be changed
+         * @param color new color
+         */
+        void setNodeColor(int node, Color color);
+
+        /**
+         * @brief Inserts a new edge from parent to child node.
+         * 
+         * @param parent parent's node
+         * @param child child's node
+         */
+        void addEdge(int parent, int child);
     
-        // function to add an edge to graph
-        void addEdge(int v, int w);
-    
-        // DFS traversal of the vertices
-        // reachable from v
-        void DFS(int v);
+        /**
+         * @brief Performs a DFS traversal of this graph starting from root node.
+         * 
+         * @param root starting point for DFS traversal
+         */
+        void DFS(int root);
+
 };
 
 
-#endif // GRAPH
+#endif // GRAPH_H
